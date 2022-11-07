332506537e957f4cefcd594f21b3a2b2901c1498
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-11-07 15:41:56 -0800

    starter frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
+
+function App() {
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    }, [dispatch]);
+
+    return (
+        <>
+            <Navigation isLoaded={isLoaded} />
+            {isLoaded && (
+                <Routes>
+                    <Route path="/login" element={<LoginFormPage />} />
+                    <Route path="/signup" element={<SignupFormPage />} />
+                    <Route path="/" element={`Home`} />
+                </Routes>
+            )}
+        </>
+    );
+}
+
+export default App;

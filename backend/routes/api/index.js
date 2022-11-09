790a9fc1699df1de3ee892b08e187a9781515d42
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-11-09 10:47:33 -0800

    GET routes for projects

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
+const projectRouter = require("./projects.js");
 
 router.use("/session", sessionRouter);
+router.use("/projects", projectRouter);
 
 module.exports = router;
---
+++ backend/routes/api/projects.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { Project } = require("../../db/models");
+const { Op } = require("sequelize");
+
+const router = express.Router();
+
+//Get all projects excluding the homepage project
+router.get(
+    "/",
+    asyncHandler(async (req, res) => {
+        const projects = await Project.findAll({
+            where: {
+                id: {
+                    [Op.not]: [1],
+                },
+            },
+        });
+
+        return res.json({
+            projects,
+        });
+    })
+);
+
+//Get specific project and it's articles
+router.get(
+    "/:projectId",
+    asyncHandler(async (req, res, next) => {
+        const id = req.params.projectId;
+
+        const project = await Project.findByPk(id);
+
+        if (!project) {
+            const err = new Error("Project not found");
+            err.status = 401;
+            err.title = "Project not found";
+            err.errors = ["The requested project doesn't exist."];
+            return next(err);
+        }
+
+        return res.json({
+            project,
+        });
+    })
+);
+module.exports = router;

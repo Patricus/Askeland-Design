5255c717759a845c99312c483d159a14e60d0547
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-11-16 15:56:40 -0800

    add articles router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const projectRouter = require("./projects.js");
+const articleRouter = require("./articles.js");
 
 router.use("/session", sessionRouter);
 router.use("/projects", projectRouter);
+router.use("/articles", articleRouter);
 
 module.exports = router;

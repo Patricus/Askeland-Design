5676d1ade3a7b325949605372a399f7d5990e81a
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-01-31 23:06:04 -0800

    Animate elementList button

--- frontend/src/components/AddToProjectButton/index.js
+++ frontend/src/components/AddToProjectButton/index.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import ElementList from "../ElementList";
+import "./AddToProjectButton.css";
 
 function AddToProjectButton({ projectId }) {
     const [showList, setShowList] = useState(false);
 
+    const toggleList = () => {
+        const buttonText = document.querySelector("#elementListText");
+
+        if (showList) buttonText.classList.remove("open");
+        else buttonText.classList.add("open");
+
+        setShowList(showList => !showList);
+    };
+
     return (
         <span style={{ position: "relative" }}>
-            <button onClick={() => setShowList(showList => !showList)}> + </button>
+            <button onClick={toggleList}>
+                <span id="elementListText"> + </span>
+            </button>
             {showList && <ElementList projectId={projectId} setShowList={setShowList} />}
         </span>
     );
